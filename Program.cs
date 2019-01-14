0063d92458e2f995545f77a27c8a3e0f98ccd65b
Bob Tabor <bobtabor@outlook.com>
2019-01-14 15:28:02 -0600

    added console code

--- Program.cs
+++ Program.cs
@@ -6,7 +6,10 @@ namespace GuessingGame
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!"); 
+            Console.WriteLine("I'm thinking of a number between 1 and 10.");
+            Console.Write("What is your guess? ");
+            Console.ReadLine();
+            Console.WriteLine("You win!");
         }
     }
 }
